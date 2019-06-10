227ad53e94808055a97fda3839d0de9b7478e239
Neeraj <n.sharma1396@gmail.com>
2019-06-10 20:09:36 +0530

    Updates

---
+++ precache-manifest.a74d693427c71fd0988b92ae975669cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e234bd515eff1e7699fef8c6890eb5ea",
+    "url": "/chartdashboard/index.html"
+  },
+  {
+    "revision": "04be68b46cea06344269",
+    "url": "/chartdashboard/static/css/2.33bbdc9a.chunk.css"
+  },
+  {
+    "revision": "435407372e94ee4493c0",
+    "url": "/chartdashboard/static/css/main.97094b4d.chunk.css"
+  },
+  {
+    "revision": "04be68b46cea06344269",
+    "url": "/chartdashboard/static/js/2.e22bb3a0.chunk.js"
+  },
+  {
+    "revision": "435407372e94ee4493c0",
+    "url": "/chartdashboard/static/js/main.abfa50d5.chunk.js"
+  },
+  {
+    "revision": "d761ac0905e305a2e868",
+    "url": "/chartdashboard/static/js/runtime~main.745f7b34.js"
+  }
+]);
